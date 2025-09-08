4d08bf183abe367780abee02e064eb0526370a3d
Paulo Figueira <pfigueira@devexperts.com>
2025-09-08 11:05:46 +0100

    Testing some functionality

--- src/main/kotlin/com/github/pjfig/testintellijplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/pjfig/testintellijplugin/toolWindow/MyToolWindowFactory.kt
@@ -10,6 +10,8 @@ import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
 import com.github.pjfig.testintellijplugin.MyBundle
 import com.github.pjfig.testintellijplugin.services.MyProjectService
+import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.dsl.builder.panel
 import javax.swing.JButton
 
 
@@ -21,7 +23,8 @@ class MyToolWindowFactory : ToolWindowFactory {
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val myToolWindow = MyToolWindow(toolWindow)
-        val content = ContentFactory.getInstance().createContent(myToolWindow.getContent(), null, false)
+        //val content = ContentFactory.getInstance().createContent(myToolWindow.getContent(), null, false)
+        val content = ContentFactory.getInstance().createContent(myToolWindow.newContent(), null, false)
         toolWindow.contentManager.addContent(content)
     }
 
@@ -30,6 +33,29 @@ class MyToolWindowFactory : ToolWindowFactory {
     class MyToolWindow(toolWindow: ToolWindow) {
 
         private val service = toolWindow.project.service<MyProjectService>()
+
+        fun newContent(): DialogPanel {
+            val panel = panel {
+                row {
+                    label(MyBundle.message("rmiHostLabel"))
+                    textField()
+                }
+                row {
+                    label(MyBundle.message("rmiPostLabel"))
+                    textField()
+
+                }
+                row{
+                    label(MyBundle.message("loginLabel"))
+                    textField()
+                }
+                row{
+                    label(MyBundle.message("passwordLabel"))
+                    textField()
+                }
+            }
+            return panel;
+        }
 
         fun getContent() = JBPanel<JBPanel<*>>().apply {
             val label = JBLabel(MyBundle.message("randomLabel", "?"))
@@ -40,6 +66,12 @@ class MyToolWindowFactory : ToolWindowFactory {
                     label.text = MyBundle.message("randomLabel", service.getRandomNumber())
                 }
             })
+            add(JBLabel(MyBundle.message("rmiHostLabel")))
+            add(JBLabel(MyBundle.message("rmiPostLabel")))
+            add(JBLabel(MyBundle.message("loginLabel")))
+            add(JBLabel(MyBundle.message("passwordLabel")))
+
+            //add(JBLabel(MyBundle.message("")))
         }
     }
 }
